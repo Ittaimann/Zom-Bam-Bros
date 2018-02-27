6f358766c92ec8edd69c9d57b28fee84796300b3
Ittaimann <ittaiam@gmail.com>
2018-02-27 15:17:56 -0800

    stuff

---
+++ project3/Assets/Scripts/EnemyScriptable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Inventory/List", order = 1)]
+public class EnemyScriptable : ScriptableObject {
+
+	public float health;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+}
---
+++ project3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	private bool fighting;
+	public float SpawnTime;
+	
+	void Awake()
+	{
+		fighting = false;
+	}
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("enemySpawn");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator enemySpawn()
+	{
+		while(!fighting)
+		{
+            yield return new WaitForSeconds(SpawnTime);
+			Debug.Log(transform.GetChild(Random.Range(0,4)).name);
+
+        }
+	}
+}
