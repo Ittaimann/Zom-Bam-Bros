25f85a1a7ca24f07dfd9246797438b4ee8c69e26
Ittaimann <ittaiam@gmail.com>
2018-02-27 17:20:23 -0800

    Added player aiming

--- project3/Assets/Scripts/Player.cs
+++ project3/Assets/Scripts/Player.cs
@@ -22,6 +22,17 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		PlayerMove();
+		aim();
+	}
+
+
+	void aim()
+	{
+		var child = transform.GetChild(1);
+		Vector2 direction=new Vector2(Input.GetAxis("RstickVertical" + playerNum), Input.GetAxis("RstickHorizontal" + playerNum));
+		direction.Normalize();
+		if(direction != Vector2.zero)
+			child.eulerAngles = new Vector3(0, 0, Mathf.Atan2(direction.x,direction.y) * 180 / Mathf.PI); 
 	}
 
 	void PlayerMove()
