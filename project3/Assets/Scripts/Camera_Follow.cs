780c1fdfef5eb05ad2824c004f837f62f6f06c18
jetsontwo <jetsontwo@gmail.com>
2018-02-27 17:04:07 -0800

    Implemented Basic Screen Split
    Working on Voronoi Screen Split but making that a stretch goal.

---
+++ project3/Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour {
+
+    public GameObject to_follow;
+    public float approach_speed, cutoff;
+    public float x_min, x_max, y_min, y_max;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diff = new Vector3(-(transform.position.x - to_follow.transform.position.x), -(transform.position.y - to_follow.transform.position.y), 0);
+        print(diff);
+        if (transform.position.x + diff.x >= x_max || transform.position.x + diff.x <= x_min)
+            diff = new Vector3(0, diff.y, 0);
+        if (transform.position.y + diff.y >= y_max || transform.position.y + diff.y <= y_min)
+            diff = new Vector3(diff.x, 0, 0);
+        if (Mathf.Abs(transform.position.magnitude - to_follow.transform.position.magnitude) > cutoff && diff != Vector3.zero)
+        {
+            print("yo");
+            transform.position += diff * approach_speed * Time.deltaTime;
+        }
+    }
+}
---
+++ project3/Assets/Scripts/Test_Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Movement : MonoBehaviour {
+
+    public KeyCode[] keys;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(keys[0]))
+            transform.Translate(Vector2.right);
+        else if (Input.GetKeyDown(keys[1]))
+            transform.Translate(-Vector2.right);
+        else if (Input.GetKeyDown(keys[2]))
+            transform.Translate(Vector2.up);
+        else if (Input.GetKeyDown(keys[3]))
+            transform.Translate(-Vector2.up);
+	}
+}
