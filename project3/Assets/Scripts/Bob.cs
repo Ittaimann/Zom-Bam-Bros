2b712982ddbe7b8d804b9b47fae9303fadb5f7fe
jetsontwo <jetsontwo@gmail.com>
2018-03-06 19:05:49 -0800

    Added Bob to Drops

--- project3/Assets/Scripts/Bob.cs
+++ project3/Assets/Scripts/Bob.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class Bob : MonoBehaviour {
 
-    private GameObject sprite;
+    public GameObject sprite;
 
 	// Use this for initialization
 	void Start () {
-        sprite = transform.GetChild(0).gameObject;
         StartCoroutine(UpandDown());
 	}
 
