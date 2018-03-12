15356fa6cbd3b635c0d0d5a9fe118850853d6a92
Ittaimann <ittaiam@gmail.com>
2018-03-12 14:43:04 -0700

    reorganized some stuff for eas of zombie art assets later

---
+++ project3/Assets/Scripts/Pair.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class DumbSpriteList: System.Object  {
+	public Sprite first;
+	public Sprite second;
+
+
+}
