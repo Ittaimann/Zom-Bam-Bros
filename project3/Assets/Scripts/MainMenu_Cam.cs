e9f8848ac4e4eb187b3f17ce9f830d0deded6641
jetsontwo <jetsontwo@gmail.com>
2018-03-14 15:31:19 -0700

    Title Screen Camera Lerp
    Also added guns to the scene so players know they are there.

---
+++ project3/Assets/Scripts/MainMenu_Cam.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu_Cam : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(25 + (45 * Mathf.Cos(Time.time/3)), 0, -10);
+	}
+}
